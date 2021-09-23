570cdff4193a8030421de1ad42a39f99add364c3
Cory Calaway <cory.c.calaway@gmail.com>
2021-09-22 18:48:04 -0600

    added json response

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -8,8 +8,20 @@ def home_view(request, *args, **kwargs):
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     print(args, kwargs)
+    """
+    REST API VIEW
+    Consume by Javascript
+    return json data
+    """
     try:
         obj = Tweet.objects.get(id=tweet_id)
     except:
         raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}!<h1>")
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}!<h1>")
+    
+
+    data = {
+
+    }
+
+    return JsonResponse(data)
